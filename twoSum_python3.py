e63cb32d602adb48f60c53792520bf9816ae28b1
yuanLeeMidori <midori3113133@gmail.com>
2022-08-04 22:26:50 -0400

    init

---
+++ twoSum_python3.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # loop through the nums list, target - nums[n]
+        print(f'target: {target}')
+        for n in range(len(nums)):
+            # if (target - nums[n]):
+            return (nums[n])
+        
+    def printNums(nums: List[int]):
+        for n in range(len(nums)):
+            print(n)
+        
+            
+
+            
+slt_Obj = Solution()
+input_nums = [1, 3 , 4, 5, 15]
+print(slt_Obj.twoSum(input_nums, 165))
+# print(slt_Obj.printNums(input_nums))
+a = [1, 2, 3, 4, 5]
+  
+# printing the list using loop
+for x in range(len(input_nums)):
+    print(input_nums[x])
