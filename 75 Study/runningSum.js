667bc912a1f310fd2969d9c37393e9be6e8222e7
yuanLeeMidori <midori3113133@gmail.com>
2022-08-13 22:42:49 -0400

    runningSum passed

---
+++ 75 Study/runningSum.js
@@ -0,0 +1,16 @@
+var runningSum = function(nums) {
+    // new array should be -
+    // every element is sum from itself to all previous one(s)
+    
+    for (let i = 1; i < nums.length; i++) {
+         
+        
+        nums[i] = nums[i] + nums[i - 1];
+
+    }
+    
+    return nums;
+};
+
+let arr = [1, 1, 1, 1];
+console.log(runningSum(arr));
